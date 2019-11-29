760ba834ae9ad6de014d27611ba150adceed083d
Taylor <contact@taylor.fish>
2019-11-29 13:22:13 -0800

    Fix stop-send-ip.sh

--- scripts/send-ip/stop-send-ip.sh
+++ scripts/send-ip/stop-send-ip.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 cd "$(dirname "$0")"
-pkill -f '\.real-send-ip\.sh DIRECTIONS-ccd92e09-bc0b-45c9-823a-27d8776e8d51\b'
+
+pkill -f \
+'\.real-send-ip\.sh DIRECTIONS-ccd92e09-bc0b-45c9-823a-27d8776e8d51\b' ||
+true
